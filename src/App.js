6020a4087d6b14529dabc76806830dbb19308037
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-09 02:07:42 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ let obj = [
   {
     name: 'Osama',
     email: 'xyz@gmail.com',
-    contact: '0900-7x22541'
+    contact: '0900-7x22541',
+    address: 'Karachi.'
   },
   {
     name: 'Anonymus',
     email: 'efg@yahoo.com',
     contact: '1245-1247548'
   }
-];
+]
 
 let name = 'Arsalan Khan'
 
@@ -24,8 +25,22 @@ const App = () => {
   return (
     <div>
       <h1>{name}</h1>
+      <h2>Names & Emails</h2>
+      {obj?.map((v, i) => {
+        return (
+          <div key={i}>
+            <span>{i + 1}) Name: {v?.name}</span>
+            <br />&nbsp;&nbsp;&nbsp;&nbsp;
+            <span>Email: <b>{v?.email}</b></span>
+            <br />&nbsp;&nbsp;&nbsp;&nbsp;
+            <span>Contact: {v?.contact}</span>
+            <br />
+            {v?.address && <span>&nbsp;&nbsp;&nbsp;&nbsp;Address: <b>{v?.address}</b></span>}
+          </div>
+        )
+      })}
     </div>
-  );
-}
+  )
+};
 
 export default App;

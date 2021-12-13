80023215f7d7fe42fade4ec295235c628d614e89
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-12 19:37:30 -0800

    Update Todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -3,14 +3,32 @@ import { useState } from 'react';
 const Todo = () => {
     const [arr, setArr] = useState([])
     const [task, setTask] = useState('')
+    
+    const addTodo = () => {
+        // console.log(task);
+        if (!task?.length) {
+            alert('Please enter Task')
+        } else {
+            arr?.push(task)
+            setArr(arr)
+            // console.log(arr);
+        }
+        setTask('')
+    }
 
     return (
         <div>
             <h1>Todo App</h1>
             <div>
-                <input name='task' value={task} placeholder='Type here...' onChange={e => setTask(e?.target?.value)}/>
-                <button>Add Todo</button>
+                <input name='task' value={task} placeholder='Type here...'
+                onChange={e => setTask(e?.target?.value)}/>
+                <button onClick={addTodo}>Add Todo</button>
             </div>
+            <ol>
+                {arr?.map((v, i) => {
+                    return <li key={i}>{v}</li>
+                })}
+            </ol>
         </div>
     )
 }

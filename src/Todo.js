44e7b259dbd367917e7d5443256e9ca918b62d20
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-12 19:14:18 -0800

    Create Todo.js

---
+++ src/Todo.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+const Todo = () => {
+    const [arr, setArr] = useState([])
+    return (
+        <div>
+            <h1>Todo App</h1>
+        </div>
+    )
+}
+
+export default Todo;

a78527c0e15ce5c57d85618616a802b731ccaf47
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-14 01:40:47 -0800

    Update Todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 const Todo = () => {
     const [arr, setArr] = useState([])
     const [task, setTask] = useState('')
+    const [isEdit, setEdit] = useState(false)
+    const [updateIndex, setIndex] = useState(null)
     
     const addTodo = () => {
         // console.log(task);
@@ -19,6 +21,26 @@ const Todo = () => {
     const deleteTodo = (index) => {
         arr?.splice(index, 1)
         setArr([...arr])
+        cancelUpdate();
+    }
+
+    const editTodo = (index) => {
+        setTask(arr[index])
+        setEdit(true)
+        setIndex(index)
+    }
+
+    const cancelUpdate = () => {
+        setEdit(false) 
+        setTask('')       
+    }
+
+    const updateTodo = () => {
+        arr[updateIndex] = task
+        setEdit(false)
+        setArr([...arr])
+        setIndex(null)
+        cancelUpdate()
     }
 
     return (
@@ -26,12 +48,17 @@ const Todo = () => {
             <h1>Todo App</h1>
             <div>
                 <input name='task' value={task} placeholder='Type here...'
-                onChange={e => setTask(e?.target?.value)}/>
-                <button onClick={addTodo}>Add Todo</button>
+                onChange={e => setTask(e?.target?.value)}/>&nbsp;
+                {!isEdit ? <button onClick={addTodo}>Add Todo</button> : <span>
+                    <button onClick={updateTodo}>Update</button>&nbsp;
+                    <button onClick={cancelUpdate}>Cancel</button>
+                    </span>}                                     
             </div>
             <ol>
                 {arr?.map((v, i) => {
                     return <li key={i}><span>{v}</span>
+                    &nbsp;&nbsp;
+                    <button onClick={() => editTodo(i)}>Edit</button>
                     &nbsp;&nbsp;
                     <button onClick={() => deleteTodo(i)}>Delete</button>
                     </li>

7663bb14f09dded8ea5306c6b34dd38f7c730767
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-18 02:20:45 -0800

    Update PropsDemo.js

--- src/Components/PropsDemo.js
+++ src/Components/PropsDemo.js
@@ -1,12 +1,33 @@
 import PropTypes from 'prop-types'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const PropsDemo = (props) => {
     // console.log(props?.message);
+    const [apiArr, setApiArr] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/photos`)
+        .then((res) => {
+            // console.log(res.data);
+            setApiArr(res?.data)
+        }, 5000)
+    }, [])
+
     return (
         <div>
             <h1>Props Message</h1>
             <p>{props?.message}</p>
+            <div>
+                {!apiArr?.length ? <p>Loading Data...</p> :
+                apiArr.map((v, i) => {
+                    return (
+                        <div key={i}>
+                            <p>{v?.title}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }

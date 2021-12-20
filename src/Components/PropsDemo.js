3a1f5b15286c8ef65ee1cdfe97cbaef13be56f89
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-20 02:42:42 -0800

    Update PropsDemo.js

--- src/Components/PropsDemo.js
+++ src/Components/PropsDemo.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 const PropsDemo = (props) => {
     // console.log(props?.message);
     const [apiArr, setApiArr] = useState([])
+    const [allArray, setAllArray] = useState([])
     const [apiCallsCount, setApiCallsCount] = useState(0)
     const [currentIndex, setCurrentIndex] = useState(0)
 
@@ -12,15 +13,28 @@ const PropsDemo = (props) => {
         setTimeout(() => {
             getData()
         }, 3000)
+    }, [])
+
+    useEffect(() => {
+        if (allArray?.length && currentIndex <= 5000) {
+            setTimeout(() => {
+            setApiArr(allArray?.slice(currentIndex, currentIndex + 10))
+            setCurrentIndex(currentIndex + 50)
+            setApiCallsCount(apiCallsCount + 1)
+            }, 300)
+        } else {
+            console.log('else...');
+        }
     }, [apiCallsCount])
 
     const getData = () => {
         axios.get(`https://jsonplaceholder.typicode.com/photos`)
             .then((res) => {
             // console.log(res.data);
-            setApiArr(res?.data?.slice(currentIndex, currentIndex + 10))
-            setCurrentIndex(currentIndex + 10)
-            setApiCallsCount(apiCallsCount + 1)
+            setApiArr(res?.data?.slice(0, 10))
+            setAllArray(res?.data)
+            setCurrentIndex(10)
+            setApiCallsCount(1)
         })
     }
 
@@ -29,6 +43,7 @@ const PropsDemo = (props) => {
             <h1>Prop Message</h1>
             <p>{props?.message}</p>
             <p>Api Calls: {apiCallsCount}</p>
+            <p>Current Index: {currentIndex}</p>
             <div>
                 {!apiArr?.length ? <p>Loading Data...</p> :
                 apiArr.map((v, i) => {

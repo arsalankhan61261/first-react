4d7d595ecbfe1001ea8851afe3b2853810a97c77
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-22 02:20:03 -0800

    Update index.js

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,9 +1,11 @@
 import Count from './Count'
 import Todo from './Todo'
 import PropsDemo from './PropsDemo'
+import Form from './form/Form'
 
 export {
     Count,
     Todo,
+    Form,
     PropsDemo
 }

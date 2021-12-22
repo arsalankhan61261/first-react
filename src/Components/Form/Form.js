464c34806769f0e3fbaa84ddee1a7c1720d88e0a
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-22 03:44:11 -0800

    Update Form.js

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 
 const Form = () => {
+    const [state, setState] = useState({
+        username: '',
+        age: '',
+    })
+
+    const onchange = (e) => {
+        setState({...state, [e?.target?.name]: e?.target?.value})
+    }
 
     const submit = (e) => {
         e?.preventdefault()
@@ -9,15 +17,16 @@ const Form = () => {
 
     return (
         <form onSubmit={submit}>
-            <h2>Add Players</h2>
+            <h2>Add Students</h2>
             <div className="form-control">
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" placeholder="Enter name here" />
+                <input type="text" name="username" id="username" value={state.username} onChange={onchange} placeholder="Enter name here" />
             </div>
             <div className="form-control">
                 <label htmlFor="age">Age</label>
-                <input type="text" id="age" placeholder="Enter age here" />
+                <input type="text" name="age" id="age" value={state.age} onChange={onchange} placeholder="Enter age here" />
             </div>
+            {/* <button type="submit">Add</button> */}
             <input type="submit" value="Add" />
         </form>
     )

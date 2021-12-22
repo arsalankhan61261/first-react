f0b04846ea06920ef35d47bddf00614ce979b07e
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-22 02:20:15 -0800

    Update Form.js

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import {  useState, useEffect } from 'react'
+import { useState } from 'react'
 
 const Form = () => {
 
@@ -24,4 +23,4 @@ const Form = () => {
     )
 }
 
-export default Form
+export default Form;

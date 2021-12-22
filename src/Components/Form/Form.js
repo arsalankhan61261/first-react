065b47300fec7210c003f625c562d6d73bacccd1
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-22 00:23:11 -0800

    Create Form.js

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Form = () => {
+
+    const submit = (e) => {
+        e?.preventdefault()
+        console.log('Submitted');
+    }
+
+    return (
+        <form onSubmit={submit}>
+            <h2>Add Players</h2>
+            <div className="form-control">
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" placeholder="Enter name here" />
+            </div>
+            <div className="form-control">
+                <label htmlFor="age">Age</label>
+                <input type="text" id="age" placeholder="Enter age here" />
+            </div>
+            <input type="submit" value="Add" />
+        </form>
+    )
+};
+
+export default Form;

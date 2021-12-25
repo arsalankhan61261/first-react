be499268971eade184186124e9e27ea461728a51
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-25 03:58:45 -0800

    Update Students.js

--- src/Components/Students/Students.js
+++ src/Components/Students/Students.js
@@ -1,9 +1,16 @@
 import { useState } from 'react';
+import { StudentItem } from '..';
 
-const Students = () => {
-
+const Students = ({students}) => {
     return (
-
+        <div>
+            <h2>Students</h2>
+            <ul>
+                {
+                    students.map((student, index) => <StudentItem key={index} index={index} student={student} />)
+                }
+            </ul>
+        </div>
     )
 }
 

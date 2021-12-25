a0a7e863b4a0d189ae783dd5d415caecbf3b0235
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-25 03:58:32 -0800

    Update Form.js

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -19,11 +19,13 @@ const Form = () => {
         <form onSubmit={submit}>
             <h2>Add Students</h2>
             <div className="form-control">
-                <label htmlFor="username">Username</label>
+                <label htmlFor="username">Username:</label>
+                &nbsp;
                 <input type="text" name="username" id="username" value={state.username} onChange={onchange} placeholder="Enter name here" />
             </div>
             <div className="form-control">
-                <label htmlFor="age">Age</label>
+                <label htmlFor="age">Age:</label>
+                &nbsp;
                 <input type="text" name="age" id="age" value={state.age} onChange={onchange} placeholder="Enter age here" />
             </div>
             <input type="submit" value="Add" />

3f2f5aac220a43dbbb2f12fcbdc5b0f21460ce4a
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-25 03:58:38 -0800

    Update index.js

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,10 +2,14 @@ import Count from './Count'
 import Todo from './Todo'
 import PropsDemo from './PropsDemo'
 import Form from './form/Form'
+import Students from './students/Students'
+import StudentItem from './students/StudentItem'
 
 export {
     Count,
     Todo,
     Form,
+    Students,
     PropsDemo,
+    StudentItem,
 }

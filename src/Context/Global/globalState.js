f78ea4aaae646e527b9f328fe93543cd6971478a
arsalankhan61261 <arsalankhan612621@gmail.com>
2021-12-27 02:54:44 -0800

    Update globalState.js

--- src/Context/Global/globalState.js
+++ src/Context/Global/globalState.js
@@ -1,1 +1,30 @@
 import { useReducer } from 'react';
+import globalContext from './globalContext';
+import globalReducer from './globalReducer';
+import { ADD_STUDNET } from '../types';
+
+const globalState = (props) => {
+    const initialState = {
+        students = []
+    }
+
+    const [state, dispatch] = useReducer(globalReducer, initialState)
+    // console.log(useReducer(globalReducer, initialState));
+
+    // Function to add Student
+    const addStudent = (newStudent) => dispatch({
+        type: ADD_STUDNET,
+        payload: newStudent
+    })
+
+    return <globalContext.Provider
+        value={{
+           students: state.students,
+           addStudent: addStudent
+        }}
+    >
+        {props.childern}
+    </globalContext.Provider>
+}
+
+export default globalState;

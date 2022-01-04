327dd22aace456821934631e3b41d1327c7f0215
arsalankhan61261 <arsalankhan612621@gmail.com>
2022-01-04 03:00:21 -0800

    Update globalReducer.js

--- src/Context/Global/globalReducer.js
+++ src/Context/Global/globalReducer.js
@@ -1,8 +1,9 @@
-import { ADD_STUDENT } from '../types';
+// import { ADD_STUDENT } from '../types';
+import { ADD_POSTS } from '../types';
 
 const globalReducer = (state, action) => {
     switch (action.type) {
-        case ADD_STUDENT:
+        case ADD_POSTS:
             return {
                 ...state,
                 students: [...state.students, action.payload] 
@@ -11,15 +12,6 @@ const globalReducer = (state, action) => {
             return {
                 ...state
             }
-        case ADD_STUDENT:
-            return {
-                ...state,
-                students: [...state.students, action.payload] 
-            }
-        default:
-            return {
-               ...state
-            };
     }
 }
 

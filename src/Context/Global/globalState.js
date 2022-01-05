25ae2995f94d858a3cc969bcf2f181f70fd73268
arsalankhan61261 <arsalankhan612621@gmail.com>
2022-01-04 23:50:00 -0800

    Update globalState.js

--- src/Context/Global/globalState.js
+++ src/Context/Global/globalState.js
@@ -2,16 +2,10 @@ import { useReducer } from 'react';
 import globalContext from './globalContext';
 import globalReducer from './globalReducer';
 import { ADD_STUDENT } from '../types';
-import { ADD_POSTS } from '../types';
 
 const GlobalState = (props) => {
-    // Initial state fro students
-    // const initialState = {
-    //     students: []
-    // }
     const initialState = {
-        posts: [],
-        page: 1,
+        students: []
     }
 
     const [state, dispatch] = useReducer(globalReducer, initialState)

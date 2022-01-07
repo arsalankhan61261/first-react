869ece36755648ef6335aa6b473f1cd833c336ed
arsalankhan61261 <arsalankhan612621@gmail.com>
2022-01-07 01:41:45 -0800

    Update globalReducer.js

--- src/Context/Posts/globalReducer.js
+++ src/Context/Posts/globalReducer.js
@@ -3,7 +3,11 @@ import { ADD_POSTS } from '../types'
 const globalReducer = (state, action) => {
     switch (action.type) {
         case ADD_POSTS:
-            return state;
+            return {
+                ...state,
+                posts: [...state.posts, ...action.payload.data],
+                page: action.payload.page + 1
+            }
         default:
             return state;
     }

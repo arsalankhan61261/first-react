efd256f9e0b5684798f1b49558139950a88c05f1
arsalankhan61261 <arsalankhan612621@gmail.com>
2022-01-27 00:03:43 -0800

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -9,18 +9,21 @@ const Login = () => {
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <div style={{ width: '80%', display: 'flex', justifyContent: 'center', backgroundColor: 'gray' }}>
+            <div style={{ width: '80%', display: 'flex', justifyContent: 'center' }}>
                 <Form
                     name="basic"
                     onFinish={onFinish}
+                    requiredMark={false}
+                    style={{ width: '50%' }}
                     // onFinishFailed={onFinishFailed}
                     >
+                        <h1 style={{ display: 'flex', justifyContent: 'center' }}>LOGIN FORM</h1>
                     <Form.Item
                         name="email"
                         rules={[
                         {
                             required: true,
-                            message: 'Please input your username!',
+                            message: 'Please input your email!',
                         },
                         {
                             type: 'email',
@@ -28,7 +31,7 @@ const Login = () => {
                         }
                         ]}
                     >
-                        <Input placeholder='Enter email here'/>
+                        <Input placeholder='Enter email here' style={{ borderRadius: '5px' }} />
                     </Form.Item>
 
                     <Form.Item
@@ -40,11 +43,11 @@ const Login = () => {
                         },
                         ]}
                     >
-                        <Input.Password placeholder='Enter password here' />
+                        <Input.Password placeholder='Enter password here' style={{ borderRadius: '5px' }} />
                     </Form.Item>
                     <Form.Item
                     >
-                        <Button type="primary" htmlType="submit">
+                        <Button type="primary" block htmlType="submit">
                         Submit
                         </Button>
                     </Form.Item>

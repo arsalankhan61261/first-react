6c1feec5f898efb0971c95e9611fc8ee1f981bb5
arsalankhan61261 <arsalankhan612621@gmail.com>
2022-02-23 04:11:54 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.css';
-import store from './Redux/store'
+import {store, persistor} from './Redux/store'
 import {Provider} from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );

6728c69978a43212f69bb859abd39ec5c398654f
arsalankhan61261 <arsalankhan612621@gmail.com>
2022-02-23 03:47:14 -0800

    Update authReducers.js

--- src/Redux/Reducers/authReducers.js
+++ src/Redux/Reducers/authReducers.js
@@ -1,9 +1,12 @@
-import { LOGIN } from '../types'
+import { LOGIN, LOGOUT } from '../types'
 
 const reducer = (state={}, action) => {
     switch (action?.type) {
         case LOGIN: {
             return {...state, user: action?.user}
+        }
+        case LOGOUT: {
+            return {...state, user: null}
         }
         default: {
             return state

130eddcb36c13e03d338980da93b5324f79069a7
arsalankhan61261 <arsalankhan612621@gmail.com>
2022-03-10 17:28:14 -0800

    Delete rootReducers.js

--- src/Redux/rootReducers.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import { authReducers, countReducer } from './Reducers'
-
-let rootReducer = combineReducers({ authReducers, countReducer })
-
-export default rootReducer;

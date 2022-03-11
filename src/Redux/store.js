3b357dcd06bb323814a335eb6b8038cece672613
arsalankhan61261 <arsalankhan612621@gmail.com>
2022-03-10 17:46:09 -0800

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,21 +1,8 @@
-import rootReducer from './rootReducers'
+import authReducers from './Reducers'
 import { createStore } from 'redux'
-import thunk from 'redux-thunk'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
 
-const persistConfig = {
-    key: 'root',
-    storage,
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-const store = createStore(persistedReducer)
-
-const persistor = persistStore(store)
+const store = createStore(authReducers)
 
 export {
-    store,
-    persistor
+    store
 }

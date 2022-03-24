c332bcd101af003d64da5877e893e3bc8433bf26
arsalankhan61261 <arsalankhan612621@gmail.com>
2022-03-23 17:09:09 -0700

    Create rootReducers.js

---
+++ src/Redux/rootReducers.js
@@ -0,0 +1,6 @@
+// import { combineReducers } from 'redux';
+// import { authReducers, countReducers } from './Reducers'
+
+// let rootReducer = combineReducers({authReducers, countReducers})
+
+// export default rootReducer;
